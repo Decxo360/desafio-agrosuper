ce80fc7b876c2785e5e328cbaf7c9a3147d4481e
Decxo360 <diegolundstedt99@gmail.com>
2023-02-03 21:40:07 -0300

    en construccion

---
+++ src/components/Pages/Cerdo.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div class="contenedor-general">
+        <h1>En construccion</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+.contenedor-general{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+    
+</style>
---
+++ src/components/Pages/Pavo.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div class="contenedor-general">
+        <h1>En Construccion</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.contenedor-general{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+    
+</style>
