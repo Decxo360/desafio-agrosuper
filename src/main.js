831eb683e51e66c0b3c9c1a38b79b04e59488fee
Decxo360 <diegolundstedt99@gmail.com>
2023-02-03 21:39:51 -0300

    uso de apexchart

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/routes'
-
-createApp(App).use(router).mount('#app')
+import VueApexCharts from 'vue3-apexcharts'
+createApp(App).use(router).use(VueApexCharts).mount('#app')
