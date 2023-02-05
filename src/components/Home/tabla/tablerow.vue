357c1ac666206e272bae0a9c8e34df6716ef0581
Decxo360 <diegolundstedt99@gmail.com>
2023-02-04 22:35:59 -0300

    arreglado errores, solucionado

--- src/components/Home/tabla/tablerow.vue
+++ src/components/Home/tabla/tablerow.vue
@@ -1,4 +1,4 @@
-<template lang="">
+<template>
         <td>{{datos.nombre_var}}</td>
         <td>{{datos.anterior}}</td>
         <td v-if="editar == false">{{datos.actual}}</td>
@@ -6,7 +6,7 @@
         <td>{{datos.variacion_porc}}%</td>
         <td><div :style="`border-radius:10px 10px 10px 10px; ${datos.status=='verde'?'background-color:green;':datos.status=='amarillo'?'background-color:yellow;':'background-color:red;'} width:15px; height:15px;`"/></td>
         <td><img width="25" src="https://img.icons8.com/metro/256/edit-file.png" alt="editar" @click="editable"></td>
-        <td><input type="checkbox" v-model="datos.checked"></td>
+        <td><input type="checkbox" v-model="datos.checked" @click="obtenerCheckBoxes"></td>
 </template>
 <script>
 export default {
@@ -15,11 +15,14 @@ export default {
             type: Object,
             required: true
         },
+        obtenerCheckBoxes:{
+            required:true
+        }
     },
     data() {
         return {
-            actual: this.datos.actual,
             editar: false,
+            actual: this.datos.actual
         }
     },
     methods: {
