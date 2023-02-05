cf5bf2f75c76977d01d777788cdc48874b905b9a
Decxo360 <diegolundstedt99@gmail.com>
2023-02-04 22:35:31 -0300

    sidebar, lugar arreglado

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -2,106 +2,79 @@
     <div class="contenedor-global">
         <div class="contenedor-header">
             <img src="https://uploads-ssl.webflow.com/5e97df93958d593cca9157a4/62c5f54f349880e24852fd07_Logo_agrosuper.png" alt="agrosuperLogo" width="300">
-            <h1>General Carnes</h1>
-            <img src="https://img.icons8.com/ios/512/user-male-circle.png" alt="icono usuario" width="150">
+            <h1>{{titulo}}</h1>
+            <img src="https://img.icons8.com/ios/512/user-male-circle.png" alt="icono usuario" width="150" @click="irPerfil">
         </div>
+        <SideBar></SideBar>
     </div>
     <div class="contenedor">
-        <SideBar></SideBar>
-        <router-view></router-view>
+        <div>
+        </div>
+        <div class="contenedor-global">
+            <div style="display:'flex';flex-direction:'wrap'; width:'100%'; justify-content:'center'; align-items:'center'">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import SideBar from './SideBar.vue';
 import axios from 'axios'
-import apexchart from "apexcharts"
+
 export default {
-    components:{
+    components: {
         SideBar,
-        apexchart
     },
-    data(){
+    data() {
+        let titulo = this.$router.currentRoute.value.name
         return{
-            proyectado:{
-                ingreso : null,
-                costo:null,
-                gav:null,
-                resultado:null
-            },
-            real:{
-                ingreso : null,
-                costo:null,
-                gav:null,
-                resultado:null
-            }
+            titulo
         }
-    },  
+    },
     methods: {
-        async getData(){
-            let response = await axios({
-                method:'post',
-                url:'http://f11.cl:8090/getCarnesRealProyeccion',
-                data:{
-                    auth:{
-                        username:"devsafio",
-                        token:"tokentokentokenABC"
-                    },
-                    query:{
-                        initial_date:{
-                            month:1,
-                            year:2020
-                        },
-                        end_date:{
-                            month:1,
-                            year:2025
-                        }
-                    }
-                },
-                headers:{
-                    "Access-Control-Allow-Origin":"*"
-                }
-            }).then((resp)=>{return resp.data})
-
-            this.real.ingreso = response.real.filter((item)=> item.concepto == "INGRESO")
-            this.real.costo = response.real.filter((item)=> item.concepto == "COSTO")
-            this.real.gav = response.real.filter((item)=> item.concepto == "GAV")
-            this.real.resultado = response.real.filter((item)=> item.concepto == "RESULTADO")
-
-            this.proyectado.ingreso = response.proyectado.filter((item)=> item.concepto == "INGRESO")
-            this.proyectado.costo = response.proyectado.filter((item)=> item.concepto == "COSTO")
-            this.proyectado.gav = response.proyectado.filter((item)=> item.concepto == "GAV")
-            this.proyectado.resultado = response.proyectado.filter((item)=> item.concepto == "RESULTADO")
-
-            console.log("proyectado", this.real)
+        irPerfil(){
+            this.$router.push({path:'Perfil'})
         }
     },
-    beforeMount() {
-        this.getData();
-    },
+    updated(){
+        this.titulo = this.$router.currentRoute.value.name
+    }
+
 }
 </script>
-<style>
+<style scoped>
+.contenedor-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 90%;
+    background: rgb(248, 244, 244);
+}
 
-    .contenedor-header{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        align-items: center;
-        width: 90%;
-        background: rgb(248, 244, 244);
-    }
-    .contenedor{
-        display: flex;
-        flex-direction: row;
-        margin-left: 94px;
-        width: 90%;
-        align-items: center;
-    }
-    .contenedor-global{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.contenedor {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-content: center;
+    margin-left: 94px;
+    width: 90%;
+    align-items: center;
+}
 
+.contenedor-global {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
 
+.router-link-active {
+    color: rgb(211, 148, 11);
+    text-decoration: none;
+}
+img:hover{
+    cursor: pointer;
+}
 </style>
